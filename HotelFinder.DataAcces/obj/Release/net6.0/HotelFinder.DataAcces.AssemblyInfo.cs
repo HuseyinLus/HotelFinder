98c2b0c1d24831b90e4a9edf13e65d298e308e02
huseyin <huseyin_lus@hotmail.com>
2024-08-07 11:13:10 +0200

    assembly commit

--- HotelFinder.DataAcces/obj/Release/net6.0/HotelFinder.DataAcces.AssemblyInfo.cs
+++ HotelFinder.DataAcces/obj/Release/net6.0/HotelFinder.DataAcces.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelFinder.DataAcces")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+272ec0ca305008916fe4a0d3eb916accaa8757af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaf1109258dd20343dda95175da4ed204eaa13f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelFinder.DataAcces")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelFinder.DataAcces")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
