361f224ea3d0c5697eb3fa8183c094b2ad4f11fc
huseyin <huseyin_lus@hotmail.com>
2024-08-07 14:28:48 +0200

    -Added Userservice

--- HotelFinder.Business/obj/Release/net6.0/HotelFinder.Business.AssemblyInfo.cs
+++ HotelFinder.Business/obj/Release/net6.0/HotelFinder.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelFinder.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bda0a404b0060e5a8d4e306f6e40956b72f4c99f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c16868381923594a7a667357c072d1dd5cc4c2fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelFinder.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelFinder.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
